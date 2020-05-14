7a13ffc1a3c4751aa26d117891099bdb5bddef9a
Sara <saragonzalezburgin@hotmail.com>
2020-05-14 17:07:32 +0200

    Declare functions

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -1,19 +1,23 @@
+console.log('it works!');
+
+// Declared with the function keyword
 // function doctorize(firstName) {
-//   return `Dr. ${firstName}`;
+//         return `Dr. ${firstName}`;
 // }
 
-// Anon Function
+// Anonymous function
 // function (firstName) {
 //   return `Dr. ${firstName}`;
 // }
 
-// Function Expression
+// Function expression
 // const doctorize = function(firstName) {
-//   return `Dr. ${firstName}`;
+//         return `Dr. ${firstName}`;
 // };
 
+// Arrow functions
 /* eslint-disable */
-const inchToCM = inches => inches * 2.54;
+const inchesToCm = (inches) => inches * 2.54;
 
 // function add(a, b = 3) {
 //   const total = a + b;
@@ -22,8 +26,7 @@ const inchToCM = inches => inches * 2.54;
 
 // const add = (a, b = 3) => a + b;
 
-// returning an object
-
+// Returning an object
 // function makeABaby(first, last) {
 //   const baby = {
 //     name: `${first} ${last}`,
@@ -34,45 +37,35 @@ const inchToCM = inches => inches * 2.54;
 
 const makeABaby = (first, last) => ({ name: `${first} ${last}`, age: 0 });
 
-
-// IIFE
-// Immediately Invoked Function Expression
-
-(function(age) {
-  return `You are cool and age ${age}`;
-})(10);
-
 // Methods!!!
+// A method is a function that lives inside of an object
+
 const wes = {
-  name: 'Westopher Bos',
-  // Method!
+  name: 'Wes Bos',
+  // Method
   sayHi: function() {
-    console.log(`Hey ${this.name}`);
     return 'Hey Wes';
   },
-  // Short hand Method
+  // MOST COMMON WAY - Short hand way to write method inside of an object
   yellHi() {
     console.log('HEY WESSSSS');
   },
   // Arrow function
-  wisperHi: () => {
-    console.log('hii wesss im a mouse');
-  }
+  whisperHi: () => console.log('hii wesss im a mouse'),
 }
 
-// Callback Functions
-// Click Callback
+// Callback functions
+// Click callback
+
 const button = document.querySelector('.clickMe');
 
 function handleClick() {
-  console.log('Great Clicking!!');
+  console.log('Great clickng!!!');
 }
 
-button.addEventListener('click', function() {
-  console.log('NIce Job!!!');
-});
+button.addEventListener('click', function() {console.log('Nice job!!!')});
 
-// Timer Callback
-setTimeout(() => {
-  console.log('DONE! Time to eat!');
-}, 1000);
+ // Timer callback
+ setTimeout(wes.yellHi, 1000);
+ setTimeout(function() {
+   console.log('done, time to eat')}, 1000);
