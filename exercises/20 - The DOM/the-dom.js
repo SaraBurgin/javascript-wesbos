40f3ee926a71367ebde9eb66a49e5eccdd672244
Sara <saragonzalezburgin@hotmail.com>
2020-05-20 16:31:57 +0200

    Select elements

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,13 @@
+const p = document.querySelector('p');
+
+const imgs = document.querySelectorAll('.item img');
+const item2 = document.querySelector('.item2');
+const item2Image = item2.querySelector('img');
+
+const h2 = document.querySelector('.item h2');
+
+console.log(h2.textContent);
+// set the textContent property on the selected element
+h2.textContent = 'Wes is cool';
+console.log(h2.textContent);
+console.log(heading.innerText);
