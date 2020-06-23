d02150ba806eff9199d5d5620e09641bac38e306
Sara <saragonzalezburgin@hotmail.com>
2020-06-23 11:35:00 +0200

    TABS AND BEDMAS

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,22 @@
-console.log('IT WORKS!');
+const terms = document.querySelector('.terms-and-conditions');
+const button = document.querySelector('.accept');
+
+// create the callback function for the observe variable
+function obCallback(payload) {
+        if (payload[0].intersectionRatio === 1) {
+                button.disabled = false;
+                ob.unobserve(terms.lastElementChild);
+        }
+}
+
+// create the observe variable and the options object
+const options = {
+        root: terms,
+        threshold: 1,
+};
+
+const ob = new IntersectionObserver(obCallback, options);
+
+// watch the observe variable
+
+ob.observe(terms.lastElementChild);
--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,26 @@
 console.log('ya ya wes we get it.. IT WORKS!');
+
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+function handleTabClick(e) {
+        // when someone clicks on the tab, hide all of the other tab panels
+        tabPanels.forEach(tabPanel => (tabPanel.hidden = true));
+        // mark all tabButtons as unselected
+        tabButtons.forEach(button => button.setAttribute('aria-selected', false));
+        // mark the clicked tab as selected
+        e.currentTarget.setAttribute('aria-selected', true);
+        // find the associated tabPanel and show it!
+        const { id } = e.currentTarget;
+        // METHOD 2 - Find in the array of tabPanels
+        const tabPanel = tabPanels.find(panel => panel.getAttribute('aria-labelledby') === id);
+        tabPanel.hidden = false;
+
+        // METHOD 1
+        // const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+        // tabPanel.hidden = false;
+}
+
+// Take our tab panels and listen for each one
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
