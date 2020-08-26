1fbca6144ee01c9c3b2c40fd2f785aa472fa33bc
Sara <saragonzalezburgin@hotmail.com>
2020-08-26 23:56:44 +0200

    Dad jokes

--- exercises/75 - CORS and Recipes/scripts.js
+++ exercises/75 - CORS and Recipes/scripts.js
@@ -1,45 +1,54 @@
 const baseEndpoint = 'http://www.recipepuppy.com/api';
-const proxy = `https://cors-anywhere.herokuapp.com/`;
+const proxy = 'https://cors-anywhere.herokuapp.com/';
 const form = document.querySelector('form.search');
 const recipesGrid = document.querySelector('.recipes');
 
 async function fetchRecipes(query) {
-  const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
-  const data = await res.json();
-  return data;
-}
-
-async function handleSubmit(event) {
-  event.preventDefault();
-  const el = event.currentTarget;
-  console.log(form.query.value);
-  fetchAndDisplay(form.query.value);
+        const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
+        const data = await res.json();
+        return data;
 }
 
 async function fetchAndDisplay(query) {
-  // turn the form off
-  form.submit.disabled = true;
-  // submit the search
-  const recipes = await fetchRecipes(query);
-  console.log(recipes);
-  form.submit.disabled = false;
-  displayRecipes(recipes.results);
+        // turn the form off
+        form.submit.disabled = true;
+        // submit the search
+        const recipes = await fetchRecipes(query);
+        console.log(recipes);
+        // turn the form on
+        form.submit.disabled = false;
+        displayRecipes(recipes.results);
+}
+
+async function handleSubmit(e) {
+        e.preventDefault();
+        const el = e.currentTarget;
+        console.log(el.query.value);
+        fetchAndDisplay(el.query.value);
 }
 
 function displayRecipes(recipes) {
-  console.log('Creating HTML');
-  const html = recipes.map(
-    recipe => `<div class="recipe">
-      <h2>${recipe.title}</h2>
-      <p>${recipe.ingredients}</p>
-      ${recipe.thumbnail &&
-        `<img src="${recipe.thumbnail}" alt="${recipe.title}"/>`}
-      <a href="${recipe.href}">View Recipe →</a>
-    </div>`
-  );
-  recipesGrid.innerHTML = html.join('');
+        console.log('Creating HTML ');
+        const html = recipes.map(
+                recipe => `<div class="recipe">
+          <h2> ${recipe.title}</h2>
+          <p>${recipe.ingredients}</p>
+          ${recipe.thumbnail && `<img src="${recipe.thumbnail}" alt="${recipe.title}"/>`}
+          <a href="${recipe.href}">View Recipe-></a>
+          </div>`
+        );
+        recipesGrid.innerHTML = html.join('');
 }
 
 form.addEventListener('submit', handleSubmit);
 // on page load run it with pizza
 fetchAndDisplay('pizza');
+
+// CORS - cross origin resource sharing. Domain names are origins, ex: github.com. By default, you are not allowed to share data between origins (you are allowed to share data inside your website but not between domains).
+
+// CORS Policy - must happen on the server. This is a policy that states that another origin (domain) can access the data and return it on their website. First of all, and most important, when you have problems with CORS, you have to get a server up and running.
+
+// Babel takes your JS and all the new terminology that isn't supported by some browsers it transpiles our code from modern JS to runable code in older browsers. Babel tries to compile async await when it is actually not necessary. ** RegeneratorRuntime is not defined. **
+
+// PROXY - used as intermediary between my localhost and the recipe puppy website, will use server side to make the request. CORS PROXY. CORS Anywhere pasted in front of my URLS and this will proxy my data for me.
+// You are sending your data through a random web server that is controlled by who knows so don't ever use it if there is sensible data.
--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -1,10 +1,48 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeButtonSpan = jokeButton.querySelector('.jokeText');
+const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
+
 const buttonText = [
-  'Ugh.',
-  '🤦🏻‍♂️',
-  'omg dad.',
-  'you are the worst',
-  'seriously',
-  'stop it.',
-  'please stop',
-  'that was the worst one',
+        'Ugh.',
+        '🤦🏻‍♂️',
+        'omg dad.',
+        'you are the worst',
+        'seriously',
+        'stop it.',
+        'please stop',
+        'that was the worst one',
 ];
+
+async function fetchJoke() {
+        // Turn loader on
+        loader.classList.remove('hidden');
+        const response = await fetch('https://icanhazdadjoke.com', {
+                headers: {
+                        Accept: 'application/json',
+                },
+        });
+        const data = await response.json();
+        // turn loader off
+        loader.classList.add('hidden');
+        return data;
+}
+
+function randomItemFromArray(arr, not) {
+        const item = arr[Math.floor(Math.random() * arr.length)];
+        // If the thing that we pass in is the last version then we don't want to return that same thing again.
+        if (item === not) {
+                console.log('Ahh we used that one last time, look again');
+                console.log(not);
+                return randomItemFromArray(arr, not);
+        }
+        return item;
+}
+
+async function handleClick() {
+        const { joke } = await fetchJoke();
+        jokeHolder.textContent = joke;
+        jokeButtonSpan.textContent = randomItemFromArray(buttonText, jokeButtonSpan.textContent);
+}
+
+jokeButton.addEventListener('click', handleClick);
